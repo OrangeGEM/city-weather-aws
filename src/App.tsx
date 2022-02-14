75e4a176b16fbd425125584fc5e644f2cb3fb097
vitalij volkov <vitalij.volkov@itp-it.com>
2022-02-14 14:34:29 +0300

    Add serverless for AWS Lambda

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,25 @@
 import axios from 'axios';
-import React, { SyntheticEvent, useState } from 'react';
+import React, { SyntheticEvent, useCallback, useState } from 'react';
 import { Container, ContentContainer, Input, InputContainer, ResultsContainer, SubmitButton, TextPart } from './styled';
 import { WeatherInterface } from './interfaces/weather.interface';
+
 const parseString = require('xml2js').parseString;
 
 function App() {
-
   const [weatherData, setWeatherData] = useState<WeatherInterface>()
   const api_key = "495dd1e62d6eba6a503427dddd78ac1b";
 
   async function handleRequest(event: React.FormEvent<HTMLFormElement>) {
     event.preventDefault();
     const target = event.currentTarget;
-    const submitter = (event.nativeEvent as any).submitter.name
     
     try {
-      if(submitter === 'search') {
-        const requestWeather = await axios({
-          'url': `https://api.openweathermap.org/data/2.5/weather?q=${target.city.value}&appid=${api_key}&units=metric&mode=xml`,
-        })
-        console.log(requestWeather);
-        parseString(requestWeather.data, {explicitArray: false, attrkey: 'data'}, (err: any, result: WeatherInterface) => {
-          setWeatherData(result);
-        })
-        
-      } else if(submitter === 'search_server') {
-        const requestWeatherFromServer = await fetch('http://localhost:3001/api/get/weather', {
-          method: 'POST',
-          body: JSON.stringify({ city: target.city.value }),
-          headers: {
-            'Content-Type': 'application/json'
-          },
-        })
-        console.log(requestWeatherFromServer);
-      }
-
+      const requestWeather = await axios({
+        url: `https://wft7rw41de.execute-api.us-east-1.amazonaws.com/default/getCurrentWeather?cityName=${target.city.value}`,
+      })
+      console.log(requestWeather.data);
     } catch(e) {
-      console.log('Request failed', e)
+      console.log(' Request error: ' + e)
     }
   }
 
@@ -48,7 +31,6 @@ function App() {
           <InputContainer style={{justifyContent:"space-between"}} onSubmit={(event) => handleRequest(event)}>
             <Input name="city"/>
             <SubmitButton name="search"> Search </SubmitButton>
-            <SubmitButton name="search_server"> Search with server </SubmitButton>
           </InputContainer>
 
           <b> Results for City Name: </b>
